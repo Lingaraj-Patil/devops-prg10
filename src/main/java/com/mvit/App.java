4fa6613cc83be9404524b25294fcc200d4fe1fd1
Manu kumar <manukumarhnm@gmail.com>
2026-04-23 18:28:03 +0530

    Intial commit

---
+++ src/main/java/com/mvit/App.java
@@ -0,0 +1,14 @@
+package com.mvit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Manu " );
+	System.out.println( "This is a print statement from the simple-app project." );
+    }
+}
